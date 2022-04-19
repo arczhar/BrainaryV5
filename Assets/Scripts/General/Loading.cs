8b19223b24177686fbfb8ff91d3f6083cf207bfa
Arczhar Mummuh <arczharjhet17@gmail.com>
2022-04-19 14:05:49 +0800

    F

--- Assets/Scripts/General/Loading.cs
+++ Assets/Scripts/General/Loading.cs
@@ -8,23 +8,24 @@ public class Loading : MonoBehaviour
     public Animator transition;
 
     public float transitionTime = 1f;
-    float time, second;
+    //float time, second;
 
 
     // Start is called before the first frame update
     void Start()
     {
-        second = 3;
-        Invoke("LoadGame", 4f);
+        //second = 3;
+        Invoke("LoadGame", 3f);
     }
 
     // Update is called once per frame
     void Update()
     {
+        /*
         if(time <5)
         {
             time += Time.deltaTime;
-        }
+        }*/
     }
 
     public void LoadGame()
